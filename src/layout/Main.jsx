3ffd5cb0f2fc7592edc95f53e408783f7a1d50bc
hbashar434 <h.bashar434@gmail.com>
2023-09-26 16:56:05 +0600

    modified tailwind.cofig.js

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 const Main = () => {
-    return (
-        <div>
-            <h1>Main Pages.</h1>
-        </div>
-    );
+  return (
+    <div>
+      <div className="min-h-[calc(100vh-88px)]">
+        <Outlet></Outlet>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      primary: ["Open Sans", "sans-serif"],
+      secondary: ["Montserrat", "sans-serif"],
+      tertiary: ["Lato", "sans-serif"],
+    },
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
